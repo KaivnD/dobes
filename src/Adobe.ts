02ca802786bee442c1af5efb0633321d4edc59c6
KaivnD <KaivnD@hotmail.com>
2019-12-18 23:14:31 +0800

    rm:event

--- src/Adobe.ts
+++ src/Adobe.ts
@@ -1,14 +1,11 @@
-import { EventEmitter } from 'events'
-
 export enum AppType {
   Illustrator,
   Photoshop
 }
 
-export class Adobe extends EventEmitter {
+export class Adobe {
   public readonly name: AppType
   public constructor(type: AppType) {
-    super()
     this.name = type
   }
 }
